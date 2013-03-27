2cb44fd7f449717b9a849f0caa08e509e8f144a1
Toby Steele <tsteele@logos.com>
2013-03-27 12:54:22 -0700

    Initial Commit

---
+++ JenkinsTestProject/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JenkinsTestProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteHelloWorldToConsole();
+            AwaitUserResponse();
+        }
+
+        private static void AwaitUserResponse()
+        {
+            Console.Write("Press any key to continue ...");
+            Console.ReadLine();
+        }
+
+        private static void WriteHelloWorldToConsole()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
