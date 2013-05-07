8ae40ebb3e91bf02fe0a44ff159c49be3938812e
Toby Steele <tsteele@logos.com>
2013-05-07 15:11:43 -0700

    Removed unimplemented method.
    Initially input as a placeholder. Won't implement, yada yada.

--- JenkinsTestProject/Program.cs
+++ JenkinsTestProject/Program.cs
@@ -5,35 +5,29 @@ using System.Text;
 
 namespace JenkinsTestProject
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            WriteHelloWorldToConsole();
-            WriteSecondaryGreetingToConsole();
-            OneMoreChange();
-            AwaitUserResponse();
-        }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			WriteHelloWorldToConsole();
+			WriteSecondaryGreetingToConsole();
+			AwaitUserResponse();
+		}
 
-        private static void OneMoreChange()
-        {
-            throw new NotImplementedException();
-        }
+		private static void WriteSecondaryGreetingToConsole()
+		{
+			Console.WriteLine("You're my homie!");
+		}
 
-        private static void WriteSecondaryGreetingToConsole()
-        {
-            Console.WriteLine("You're my homie!");
-        }
+		private static void AwaitUserResponse()
+		{
+			Console.Write("Press any key to continue ...");
+			Console.ReadLine();
+		}
 
-        private static void AwaitUserResponse()
-        {
-            Console.Write("Press any key to continue ...");
-            Console.ReadLine();
-        }
-
-        private static void WriteHelloWorldToConsole()
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+		private static void WriteHelloWorldToConsole()
+		{
+			Console.WriteLine("Hello, World!");
+		}
+	}
 }
